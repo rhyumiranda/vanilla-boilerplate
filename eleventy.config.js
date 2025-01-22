624ab7b451c6e22b6d7858cbd2a9d0b8dad5909b
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2025-01-22 23:14:22 +0800

    chore: install dev dependencies

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,22 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/styles.css");
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    formats: ["avif", "webp", "jpeg"],
+    widths: ["auto"],
+    htmlOptions: {
+      imgAttributes: {
+        loading: "lazy",
+        decoding: "async",
+      },
+      pictureAttributes: {}
+    }
+
+  });
+
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addPassthroughCopy("./src/js");
+
 
   return {
     dir: {
